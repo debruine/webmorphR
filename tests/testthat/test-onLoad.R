888e091ab7cf890143b4c26140808430a4b933b7
debruine <debruine@gmail.com>
2022-05-18 21:53:54 +0100

    test onload function

---
+++ tests/testthat/test-onLoad.R
@@ -0,0 +1,18 @@
+test_that(".onLoad", {
+  # remove all opts
+  opts <- wm_opts()
+  lapply(opts, function(x) { NULL } ) |> wm_opts()
+  
+  # set opt to non-default value
+  wm_opts(fill = "black")
+  
+  # keep non-defaults on load
+  webmorphR:::.onLoad()
+  opts <- wm_opts()
+  expect_equal(opts$fill, "black")
+})
+
+# onAttach ----
+test_that("onAttach", {
+  expect_message(webmorphR:::.onAttach())
+})
